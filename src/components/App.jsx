ad658f788a94ad31c326b38b0478de83324160ab
linuxmobile <bdiez19@gmail.com>
2023-04-04 15:45:22 -0300

    Feat 💎: adding tailwindcss, reactRefresh & more

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import UseAppRecommendation from './UseAppRecommendation.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="">
+      <UseAppRecommendation />
     </div>
   )
 }
