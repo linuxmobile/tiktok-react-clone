925e7f07a47275bb183479b86118fb896ac0a761
linuxmobile <bdiez19@gmail.com>
2023-04-05 21:01:54 -0300

    Feat 💎: improved pause & play function

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,16 +1,18 @@
 import { useState, useRef, useEffect } from 'react'
 import TiktokVideo from '../assets/videos/tiktok1.mp4'
-import PauseIcon from '../assets/pause.png'
 
 function VideoPlayer() {
   const [playing, setPlaying] = useState(false)
   const video = useRef()
+  const btn = useRef()
 
   const handlePlay = () => {
     if (playing) {
       video.current.pause()
+      btn.current.classList.remove('opacity-0')
     } else {
       video.current.play()
+      btn.current.classList.add('opacity-0')
     }
     setPlaying(!playing)
   }
@@ -20,12 +22,11 @@ function VideoPlayer() {
         src={TiktokVideo}
         ref={video}
         controls={false}
-        className='w-[353px]'
+        className='w-full h-full'
+        onClick={handlePlay}
       >
       </video>
-      <div className='w-full h-full'>
-        <button onClick={handlePlay} className='w-12 h-12 absolute top-72 left-36'><img src={PauseIcon} alt="pause image" /></button>
-      </div>
+        <button ref={btn} onClick={handlePlay} className='absolute bg-[url(pause.png)] z-10 inset-0 m-auto w-16 h-16 bg-contain bg-no-repeat transition-all ease-in-out duration-300'></button>
 		</div>
 	)
 }
